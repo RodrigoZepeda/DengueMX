56da8a46ef4fd36193885d523bc726bdeaa6641d
Rodrigo Zepeda-Tello <rzepeda17@gmail.com>
2022-06-10 16:38:21 -0500

    Actualización 2022-06-10

--- orchestrate.sh
+++ orchestrate.sh
@@ -1,9 +1,9 @@
 . ~/.keychain/`/bin/hostname`-sh
 cd /home/rodrigo/DengueMX
 date=$(date '+%Y-%m-%d')
-/home/rodrigo/miniconda3/envs/DengueMX/bin/python3 /home/rodrigo/DengueMX/scripts/Descarga.py
-/usr/bin/R < /home/rodrigo/DengueMX/scripts/format_dataset.R --no-save
-/home/rodrigo/miniconda3/envs/DengueMX/bin/python3 /home/rodrigo/DengueMX/scripts/model_darts.py
+/home/rodrigo/miniconda3/envs/Dengue/bin/python3 /home/rodrigo/Dengue/scripts/Descarga.py
+/usr/bin/R < /home/rodrigo/Dengue/scripts/format_dataset.R --no-save
+/home/rodrigo/miniconda3/envs/Dengue/bin/python3 /home/rodrigo/Dengue/scripts/model_darts.py
 /usr/bin/git -C /home/rodrigo/DengueMX add .
 /usr/bin/git -C /home/rodrigo/DengueMX commit -m "Actualización ${date}"
 /usr/bin/git -C /home/rodrigo/DengueMX push origin main
