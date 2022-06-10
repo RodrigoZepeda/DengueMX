1584e07dea4bde073c2c6eb4f6904b81f209eaef
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2022-06-10 17:35:15 -0500

    New model just dropped

--- scripts/model_darts.py
+++ scripts/model_darts.py
@@ -20,6 +20,8 @@ logging.disable(logging.CRITICAL)
 data           = pd.read_csv("datos-limpios/dengue_2015_2022_mx.csv")
 data           = data.rename(columns = {"fecha": "date", "logn": "value"})
 data['date']   = pd.to_datetime(data['date'])
+data = data[lambda x: data.date.max() > x.date]
+data = data[lambda x: x.date > pd.Timestamp("2015/12/31")]
 
 # Create a TimeSeries, specifying the time and value columns
 series = TimeSeries.from_dataframe(data[["date","value"]], 'date', 'value')
@@ -29,34 +31,13 @@ training_cutoff = data.date.max() - timedelta(weeks=weeks_to_predict)
 
 # Set aside the last two year as a validation series
 train, val = series.split_after(training_cutoff)
-#train.plot()
-#val.plot()
-
-#COVARIATES
-covariates = datetime_attribute_timeseries(series, attribute="year", one_hot=False)
-covariates = covariates.stack(
-    datetime_attribute_timeseries(series, attribute="month", one_hot=True)
-)
-covariates = covariates.stack(
-    TimeSeries.from_times_and_values(
-        times=series.time_index,
-        values=np.arange(len(series)),
-        columns=["linear_increase"],
-    )
-)
-
-#TRANSFORM COVARIATES
-scaler_covs = Scaler()
-cov_train, cov_val = covariates.split_after(training_cutoff)
-scaler_covs.fit(cov_train)
-covariates_transformed = scaler_covs.transform(covariates)
 
 # reproducibility
 np.random.seed(230098)
 torch.manual_seed(42095703)
 
 deeptcn = NHiTS(
-    input_chunk_length=100,
+    input_chunk_length=120,
     output_chunk_length=10,
     num_stacks=3,
     num_blocks=1,
@@ -64,7 +45,7 @@ deeptcn = NHiTS(
     num_layers=5,
     layer_widths=512,
     batch_size=16,
-    n_epochs=20,
+    n_epochs=100,
     likelihood = Likelihood.GammaLikelihood(),
     loss_fn = Loss.SmapeLoss(),
     #optimizer_kwargs = {'lr': 1e-5},
@@ -80,13 +61,13 @@ deeptcn.fit(train, verbose=True)
 
 #Future predict
 deeptcn.fit(series, verbose=True)
-pred_2     = deeptcn.predict(weeks_to_predict, num_samples=1000)
+pred_2     = deeptcn.predict(weeks_to_predict, num_samples=10000)
 
-prediction = pred_2.quantiles_df([0.05, 0.5, 0.95])
-prediction[["lower_ci", "median", "upper_ci"]] = np.exp(prediction[["value_0.05", "value_0.5", "value_0.95"]])
+prediction = pred_2.quantiles_df([0.005, 0.5, 0.995])
+prediction[["lower_ci", "median", "upper_ci"]] = np.exp(prediction[["value_0.005", "value_0.5", "value_0.995"]])
 
 
-pred_2.plot()
+#pred_2.plot()
 prediction.to_csv("predicciones_DARTS_experimento.csv")
 
 try:
--- scripts/plot_results.R
+++ scripts/plot_results.R
@@ -1,4 +1,5 @@
-rm(list = ls())
+#!/usr/bin/env Rscript
+
 pacman::p_load(bsts, tidyverse, lubridate, glue, ggtext, cmdstanr, reticulate)
 
 #Limpieza de la base
@@ -6,15 +7,18 @@ experimento <- read_csv("predicciones_DARTS_experimento.csv")
 dengue_all  <- read_csv("datos-limpios/dengue_for_model_mx.csv")
 
 ggplot(experimento) +
-  geom_line(aes(x = date, y = median), data = experimento, color = "black") +
+  geom_line(aes(x = fecha, y = n, color = "Predicho", size = "Predicho"),
+           data = dengue_all) +
+  geom_line(aes(x = date, y = median, color = "Predicho",
+                linetype = "Predicho", size = "Predicho"), data = experimento) +
   geom_ribbon(aes(x = date, ymin = lower_ci, y = median, ymax = upper_ci), alpha = 0.25,
               fill = "#12757E", data = experimento) +
   #geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci), alpha = 0.25,
   #            fill = "#12757E") +
   #geom_ribbon(aes(ymin = model2_low, ymax = model2_up), alpha = 0.25,
   #            fill = "red") +
-  geom_line(aes(color = "Observado", linetype = "Observado", size = "Observado",
-                x = fecha, y = nraw), data = dengue_all) +
+  geom_point(aes(color = "Observado", linetype = "Observado", size = "Observado",
+                x = fecha, y = nraw), data = dengue_all, alpha = 0.5) +
  #geom_line(aes(color = "red", size = color, y = model2)) +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
   theme_minimal() +
@@ -23,13 +27,14 @@ ggplot(experimento) +
     y = "Casos probables",
     title = glue::glue("<span style = 'color:#92AF75;'>Casos probables de dengue</span> ",
                        "en México por fecha de inicio de síntomas"),
-    caption = glue::glue("Elaborada el {today()}"),
-    subtitle = glue::glue("Fuente: Datos Abiertos de la Secretaría de Salud y ",
-                          "Panoramas Epidemiológicos de Dengue 2017-2019")
+    caption = glue::glue("Elaborada el {today()}.\nFuente: Datos Abiertos de la Secretaría de Salud 2020-{year(today())} y ",
+                         "Panoramas Epidemiológicos de Dengue 2017-2019."),
+    subtitle = glue::glue("Modelo de interpolación jerárquica neuronal (N-HiTS) | Github: RodrigoZepeda/DengueMX")
   ) +
   scale_y_continuous(labels = scales::comma) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1),
         plot.title = element_markdown(),
+        plot.caption = element_text(size = 6),
         panel.background = element_rect(fill = "#FBFFFB"),
         plot.background  = element_rect(fill = "#FBFFFB"),
         plot.subtitle    = element_text(size = 8, face = "italic", color = "gray25")) +
