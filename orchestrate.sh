83d8c39ad47887cff8f249256c3b912eb799d460
Rodrigo Zepeda-Tello <rzepeda17@gmail.com>
2022-06-30 13:23:58 -0500

    Actualización

--- orchestrate.sh
+++ orchestrate.sh
@@ -1,9 +1,20 @@
 . ~/.keychain/`/bin/hostname`-sh
 cd /home/rodrigo/DengueMX
 date=$(date '+%Y-%m-%d')
+
+#Descarga de el más reciente archivo de dengue
 /home/rodrigo/miniconda3/envs/Dengue/bin/python3 /home/rodrigo/DengueMX/scripts/Descarga.py
+
+#Formateo de la base de dengue
 /usr/bin/R < /home/rodrigo/DengueMX/scripts/format_dataset.R --no-save
+
+#Descarga del más reciente archivo de clima
+/usr/bin/R < /home/rodrigo/DengueMX/datos-clima/descarga_clima_1985_today.R --no-save
+
+#Correr el modelo
 /usr/bin/R < /home/rodrigo/DengueMX/scripts/model_bayes.R --no-save
+
+#Subir a Github
 /usr/bin/git -C /home/rodrigo/DengueMX add .
 /usr/bin/git -C /home/rodrigo/DengueMX commit -m "Actualización ${date}"
 /usr/bin/git -C /home/rodrigo/DengueMX push origin lognormal
